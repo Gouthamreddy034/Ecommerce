a5d4e3d8ca5d8d3328e0aacfcf8d2acaaa449c90
Yanala Goutham Reddy <lakshmareddyyanala@gmail.com>
2024-08-18 08:56:00 +0530

    Added Wishlist Entity

---
+++ src/main/java/com/yanala/ecommerce/entity/Wishlist.java
@@ -0,0 +1,24 @@
+package com.yanala.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Data
+@Entity
+public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name="user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name="product_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Product product;
+}
